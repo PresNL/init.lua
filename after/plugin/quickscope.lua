9feb1b9510d7e5526e4e054d372c8d3801b1af3c
Rodi Lankester <Rodi@lumion.com>
2023-11-14 12:12:31 +0100

    Fix quickscope colors not being set on startup

--- after/plugin/quickscope.lua
+++
@@ -1,6 +0,0 @@
-function UpdateColors()
-    vim.api.nvim_set_hl(0, 'QuickScopePrimary', {fg='#afff5f', gui=underline, ctermfg=155})
-    vim.api.nvim_set_hl(0, 'QuickScopeSecondary', {fg='#8ccc4b', gui=underline, ctermfg=81})
-end
-
-UpdateColors()
